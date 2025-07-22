8319bbc233507fde0751bde8d2d74a400cc42db1
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-07-22 01:59:04 +0100

    feat(packages): add rewards section:

---
+++ packages/babylon-core-ui/src/widgets/sections/RewardSubsection/RewardSubsection.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { RewardSubsection } from "./RewardSubsection";
+
+const meta: Meta<typeof RewardSubsection> = {
+    component: RewardSubsection,
+    tags: ["autodocs"],
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        amount: 123,
+        currencyIcon: "https://placehold.co/40x40",
+        currencyName: "BBN",
+        displayBalance: true,
+        chainName: "Babylon",
+        balanceDetails: {
+            balance: 1234.56789,
+            symbol: "BBN",
+            price: 0.25,
+            displayUSD: true,
+            decimals: 8,
+        },
+    },
+}; 
