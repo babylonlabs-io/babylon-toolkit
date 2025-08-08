40fd47c6c0c9f294f7f71cdc7306b957627729ac
jeremy-babylonlabs <jeremy.t@babylonlabs.io>
2025-08-09 04:27:19 +0800

    fix(packages): storybook config

--- packages/babylon-campaigns/.storybook/main.ts
+++ packages/babylon-campaigns/.storybook/main.ts
@@ -1,10 +1,10 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "storybook-dark-mode",
   ],
@@ -12,9 +12,5 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
-  typescript: {
-    reactDocgen: "react-docgen-typescript",
-  },
 };
-
 export default config;
