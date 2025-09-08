b44c719a5fba3a328ec3aff53e1de53bcf2b694f
necipsagiro <sagiroglun22@itu.edu.tr>
2025-09-08 12:09:01 +0300

    refactor(packages): rename variable

--- services/simple-staking/src/ui/baby/widgets/StakingForm/index.tsx
+++ services/simple-staking/src/ui/baby/widgets/StakingForm/index.tsx
@@ -66,9 +66,9 @@ export default function StakingForm({
       className="flex h-[500px] flex-col gap-2"
       onSubmit={handlePreview}
       defaultValues={defaultValues}
-      onChange={(a) => setBabyStakeDraft({
-        ...a,
-        validatorAddresses: a.validatorAddresses?.filter(i => i !== undefined),
+      onChange={data => setBabyStakeDraft({
+        ...data,
+        validatorAddresses: data.validatorAddresses?.filter(i => i !== undefined),
       })}
     >
       <AmountField balance={availableBalance} price={babyPrice} />
