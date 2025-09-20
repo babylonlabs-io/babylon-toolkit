5a5a27017ec78b2f3e7b191cb18352738a09c83a
Tong Li <tongli0809@gmail.com>
2025-09-20 11:07:01 +1000

    feat(build): bump vite for babylon-wallet-connector

--- packages/babylon-campaigns/storybook-static/eligibility-dashboard/EligibilityDashboard.d.ts
+++
@@ -1,5 +0,0 @@
-import { default as React } from '../../../../node_modules/react';
-export interface EligibilityDashboardProps {
-    className?: string;
-}
-export declare const EligibilityDashboard: React.FC<EligibilityDashboardProps>;
--- packages/babylon-campaigns/storybook-static/eligibility-dashboard/context/EligibilityDashboard.context.d.ts
+++
@@ -1,9 +0,0 @@
-import { default as React } from '../../../../../node_modules/react';
-export interface EligibilityState {
-    isEligible: boolean;
-    setEligible: React.Dispatch<React.SetStateAction<boolean>>;
-}
-export declare const EligibilityProvider: React.FC<{
-    children: React.ReactNode;
-}>;
-export declare const useEligibilityContext: () => EligibilityState;
