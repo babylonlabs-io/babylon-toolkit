74deef731ee40497b3f770460d01a16075bb8b31
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-09-22 23:54:41 +0100

    feat(packages): lint

--- services/simple-staking/src/ui/common/components/Banner/Banner.tsx
+++ services/simple-staking/src/ui/common/components/Banner/Banner.tsx
@@ -7,28 +7,27 @@ import { network as bbnNetwork } from "@/ui/common/config/network/bbn";
 
 export const Banner = () => {
   const shouldDisplayTestingMessage = shouldDisplayTestingMsg();
-  const shouldDisplaySunsettingMessage = FeatureFlags.IsTestnetSunsetEnabled && bbnNetwork === "testnet";
+  const shouldDisplaySunsettingMessage =
+    FeatureFlags.IsTestnetSunsetEnabled && bbnNetwork === "testnet";
 
   if (!shouldDisplayTestingMessage && !shouldDisplaySunsettingMessage) {
-    return null
+    return null;
   }
 
   return (
     <div className="flex flex-col">
-      {shouldDisplayTestingMessage &&
+      {shouldDisplayTestingMessage && (
         <div className="flex flex-row items-center justify-between gap-2 bg-[#D5FCE8] px-4 py-3 text-primary-main">
           <div className="flex flex-row items-center gap-2">
             <PiWarningOctagonFill />
             <Text variant="body1">
               <strong>This is a testing app</strong>
               <br />
-              The app may contain bugs. Use it after conducting your own research
-              and making an informed decision. Tokens are for testing only and do
-              not carry any monetary value and the testnet is not incentivized.
+              The app may contain bugs. Use it after conducting your own research and making an informed decision. Tokens are for testing only and do not carry any monetary value and the testnet is not incentivized.
             </Text>
           </div>
         </div>
-      }
+      )}
 
       {shouldDisplaySunsettingMessage && (
         <div className="flex flex-row items-center justify-between gap-2 bg-[#FFF7D6] px-4 py-3 text-primary-main">
