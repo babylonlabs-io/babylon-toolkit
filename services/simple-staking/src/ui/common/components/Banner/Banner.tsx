7ba3cab4f4e2f1d90d13950d10683f4a826852f0
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-09-22 23:45:34 +0100

    feat(packages): lint

--- services/simple-staking/src/ui/common/components/Banner/Banner.tsx
+++ services/simple-staking/src/ui/common/components/Banner/Banner.tsx
@@ -6,26 +6,31 @@ import FeatureFlags from "@/ui/common/utils/FeatureFlagService";
 import { network as bbnNetwork } from "@/ui/common/config/network/bbn";
 
 export const Banner = () => {
-  if (!shouldDisplayTestingMsg()) {
-    return null;
+  const shouldDisplayTestingMessage = shouldDisplayTestingMsg();
+  const shouldDisplaySunsettingMessage = FeatureFlags.IsTestnetSunsetEnabled && bbnNetwork === "testnet";
+
+  if (!shouldDisplayTestingMessage && !shouldDisplaySunsettingMessage) {
+    return null
   }
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-row items-center justify-between gap-2 bg-[#D5FCE8] px-4 py-3 text-primary-main">
-        <div className="flex flex-row items-center gap-2">
-          <PiWarningOctagonFill />
-          <Text variant="body1">
-            <strong>This is a testing app</strong>
-            <br />
-            The app may contain bugs. Use it after conducting your own research
-            and making an informed decision. Tokens are for testing only and do
-            not carry any monetary value and the testnet is not incentivized.
-          </Text>
+      {shouldDisplayTestingMessage &&
+        <div className="flex flex-row items-center justify-between gap-2 bg-[#D5FCE8] px-4 py-3 text-primary-main">
+          <div className="flex flex-row items-center gap-2">
+            <PiWarningOctagonFill />
+            <Text variant="body1">
+              <strong>This is a testing app</strong>
+              <br />
+              The app may contain bugs. Use it after conducting your own research
+              and making an informed decision. Tokens are for testing only and do
+              not carry any monetary value and the testnet is not incentivized.
+            </Text>
+          </div>
         </div>
-      </div>
+      }
 
-      {FeatureFlags.IsTestnetSunsetEnabled && bbnNetwork === "testnet" && (
+      {shouldDisplaySunsettingMessage && (
         <div className="flex flex-row items-center justify-between gap-2 bg-[#FFF7D6] px-4 py-3 text-primary-main">
           <div className="flex flex-row items-center gap-2">
             <PiWarningOctagonFill />
