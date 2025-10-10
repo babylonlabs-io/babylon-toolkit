cfd1d50647ead87a76a5eabc4582bd00642b072b
jeremy-babylonlabs <jeremy.t@babylonlabs.io>
2025-10-10 16:49:25 +0700

    feat(packages): vault file structure bootstrap

--- routes/vault/src/index.ts
+++ routes/vault/src/index.ts
@@ -1,3 +1,14 @@
 export { default as VaultLayout } from "./VaultLayout";
+
+// Components
 export * from "./components";
+
+// Types
 export * from "./types";
+
+// Future exports - uncomment as implemented
+// export * from "./clients";
+// export * from "./config";
+// export * from "./hooks";
+// export * from "./services";
+// export * from "./utils";
