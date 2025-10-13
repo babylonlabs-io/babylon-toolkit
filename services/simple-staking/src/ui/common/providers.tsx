144b7ddf43731c1591fa1295391d749959a28325
necipsagiro <sagiroglun22@itu.edu.tr>
2025-10-14 00:52:46 +0300

    chore(packages): lint

--- services/simple-staking/src/ui/common/providers.tsx
+++ services/simple-staking/src/ui/common/providers.tsx
@@ -4,6 +4,8 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "next-themes";
 import { Suspense, useEffect, useRef, useState } from "react";
 
+import { PendingOperationsProvider } from "@/ui/baby/hooks/services/usePendingOperationsService";
+
 import { NotificationContainer } from "./components/Notification/NotificationContainer";
 import { ErrorProvider } from "./context/Error/ErrorProvider";
 import { BbnRpcProvider } from "./context/rpc/BbnRpcProvider";
@@ -11,7 +13,6 @@ import { BTCWalletProvider } from "./context/wallet/BTCWalletProvider";
 import { CosmosWalletProvider } from "./context/wallet/CosmosWalletProvider";
 import { WalletConnectionProvider } from "./context/wallet/WalletConnectionProvider";
 import { AppState } from "./state";
-import { PendingOperationsProvider } from "@/ui/baby/hooks/services/usePendingOperationsService";
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = useState(new QueryClient());
