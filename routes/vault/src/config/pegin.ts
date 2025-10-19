a4cb2696fe73bb4052c9a0c32340164f7a585f3a
Crypto Minion <154598612+jrwbabylonlab@users.noreply.github.com>
2025-10-19 14:05:14 +1100

    update the hardcoded liquidator PK (#444)

--- routes/vault/src/config/pegin.ts
+++ routes/vault/src/config/pegin.ts
@@ -39,14 +39,14 @@ export function getBTCNetworkForWASM(): WASMNetwork {
 // TODO: To be replaced by calling the backend API to get liquidators
 export const LOCAL_PEGIN_CONFIG = {
   /**
-   * HARDCODED: Local liquidators (challengers) BTC public keys
-   * These are the two liquidators configured in btc-vault-deployment
-   * From: LIQUIDATOR_1_BTC_PUBKEY and LIQUIDATOR_2_BTC_PUBKEY
+   * HARDCODED: Vault-devnet liquidators (challengers) BTC public keys
+   * These are the two liquidators configured in vault-devnet deployment
+   * From: L1_BTC_PUBKEY and L2_BTC_PUBKEY (vault-devnet)
    * TODO: Fetch from backend API
    */
   liquidatorBtcPubkeys: [
-    '79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798', // Liquidator 1
-    'c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5', // Liquidator 2
+    'e288d2c35ffa4c3930004c81de604b5000a9ca06ce55faaf6ac092de309f9373', // L1 (Liquidator 1)
+    'bb6d11b89db08fe70f1cd4293468c85cae62e57316546db9651c6e1244724192', // L2 (Liquidator 2)
   ],
 
   /**
