196f3081b14041096853424767ca4052386aef9c
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-10-23 18:08:31 +0200

    feat(packages): fix routes

--- services/super-app/src/ui/router.tsx
+++ services/super-app/src/ui/router.tsx
@@ -1,18 +1,13 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 
-import NotFound from "./common/not-found";
 import { StakingApp } from "./staking/StakingApp";
 import { VaultApp } from "./vault/VaultApp";
 
 export const Router = () => {
     return (
         <Routes>
-            <Route index element={<Navigate to="btc" replace />} />
-            <Route path="btc/*" element={<StakingApp />} />
-            <Route path="baby/*" element={<StakingApp />} />
-            <Route path="rewards/*" element={<StakingApp />} />
-            <Route path="vault/*" element={<VaultApp />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path="/vault/*" element={<VaultApp />} />
+            <Route path="/*" element={<StakingApp />} />
         </Routes>
     );
 };
