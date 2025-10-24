b0e4bf56b5b4643249bd2cb0a94248c6ca0bf4f0
wjrjerome <wjrjerome@babylonchain.io>
2025-10-24 17:38:36 +1100

    chore(packages): remove the super app and re-organise the file structure a bit

---
+++ services/vault/src/index.ts
@@ -0,0 +1,5 @@
+// Vault deposit flow UI components
+export * from "./assets";
+export * from "./components";
+export * from "./state/VaultDepositState";
+export { VaultLayout } from "./components/pages/VaultLayout";
