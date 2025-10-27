5ae754e32c9ef359d229e0dad5cc460799e782a2
wjrjerome <wjrjerome@babylonchain.io>
2025-10-27 12:05:19 +0800

    fix(packages): disable import resolution errors for external dependencies

--- services/vault/src/utils/btc/wasm.ts
+++ services/vault/src/utils/btc/wasm.ts
@@ -1,7 +1,7 @@
 import init, {
   WasmPeginPayoutConnector,
   WasmPeginTx,
-} from "@routes/vault/wasm/btc_vault.js";
+} from "@routes/vault/wasm/btc_vault.js"; // eslint-disable-line import-x/no-unresolved
 
 let wasmInitialized = false;
 let wasmInitPromise: Promise<void> | null = null;
@@ -146,4 +146,4 @@ export async function createPayoutConnector(
 export {
   WasmPeginPayoutConnector,
   WasmPeginTx,
-} from "@routes/vault/wasm/btc_vault.js";
+} from "@routes/vault/wasm/btc_vault.js"; // eslint-disable-line import-x/no-unresolved
