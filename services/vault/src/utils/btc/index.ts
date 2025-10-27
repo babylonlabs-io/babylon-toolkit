d681377378e42a077269fb1376aea86188f9939a
wjrjerome <wjrjerome@babylonchain.io>
2025-10-27 12:38:18 +0800

    fix(packages): add missing dependencies and exports for services

--- services/vault/src/utils/btc/index.ts
+++ services/vault/src/utils/btc/index.ts
@@ -6,8 +6,20 @@
 
 export { BitcoinScriptType, getScriptType } from "./btcScriptType";
 export { processPublicKeyToXOnly, stripHexPrefix, toXOnly } from "./btcUtils";
+export { TAP_INTERNAL_KEY, tapInternalPubkey } from "./constants";
 export {
   getPsbtInputFields,
   type PsbtInputFields,
   type UTXO,
 } from "./getPsbtInputFields";
+export {
+  WasmPeginPayoutConnector,
+  WasmPeginTx,
+  createPayoutConnector,
+  createPegInTransaction,
+  type Network,
+  type PayoutConnectorInfo,
+  type PayoutConnectorParams,
+  type PegInParams,
+  type PegInResult,
+} from "./wasm";
