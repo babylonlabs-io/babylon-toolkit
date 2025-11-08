c83e993cbf78be0a458fba60be8962e4921c7f5a
wjrjerome <wjrjerome@babylonchain.io>
2025-11-08 16:42:58 +1100

    fix(packages): handle staking expansion new event and contract id

--- packages/babylon-wallet-connector/src/core/utils/contracts.ts
+++ packages/babylon-wallet-connector/src/core/utils/contracts.ts
@@ -1,6 +1,7 @@
 // Can be found from https://github.com/unisat-wallet/bitcoin-address-verifier/blob/master/src/plugins/babylon/types/types.ts
 export const BABYLON_SIGNING_CONTRACTS = {
   STAKING: "babylon:staking",
+  STAKING_EXPANSION: "babylon:staking-expansion",
   UNBONDING: "babylon:unbonding",
   SLASHING: "babylon:slashing",
   SLASHING_BURN: "babylon:slashing-burn",
