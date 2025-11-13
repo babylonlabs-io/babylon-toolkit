b9671211b4154b51b67021884e087c4c4808e595
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-11-13 01:23:48 +0100

    feat(packages): fix test

--- services/vault/vitest.config.ts
+++ services/vault/vitest.config.ts
@@ -15,6 +15,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/integration.test.tsx',
+    ],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
