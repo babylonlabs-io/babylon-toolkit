56e4eaf44b084636731a82ae119a8b67a596b464
wjrjerome <wjrjerome@babylonchain.io>
2025-11-20 17:29:20 +1100

    fix(packages): bump btc-staking-ts to include stricter staking input validation

--- services/simple-staking/tests/utils/featureFlag/FeatureFlagService.test.ts
+++ services/simple-staking/tests/utils/featureFlag/FeatureFlagService.test.ts
@@ -15,6 +15,7 @@ describe("FeatureFlagService", () => {
 
   describe("IsLedgerEnabled", () => {
     it("should return false when NEXT_PUBLIC_FF_ENABLE_LEDGER is not set", () => {
+      delete process.env.NEXT_PUBLIC_FF_ENABLE_LEDGER;
       expect(FeatureFlagService.IsLedgerEnabled).toBe(false);
     });
 
@@ -31,6 +32,7 @@ describe("FeatureFlagService", () => {
 
   describe("IsCoStakingEnabled", () => {
     it("should return false when NEXT_PUBLIC_FF_CO_STAKING is not set", () => {
+      delete process.env.NEXT_PUBLIC_FF_CO_STAKING;
       expect(FeatureFlagService.IsCoStakingEnabled).toBe(false);
     });
 
@@ -47,6 +49,7 @@ describe("FeatureFlagService", () => {
 
   describe("IsTimelockRenewalEnabled", () => {
     it("should return false when NEXT_PUBLIC_FF_TIMELOCK_RENEWAL is not set", () => {
+      delete process.env.NEXT_PUBLIC_FF_TIMELOCK_RENEWAL;
       expect(FeatureFlagService.IsTimelockRenewalEnabled).toBe(false);
     });
 
