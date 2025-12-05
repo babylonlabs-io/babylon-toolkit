a0cc80d8eb526f1091027ce106e07c97eec1cac1
Tong Li <supertong@users.noreply.github.com>
2025-12-05 13:00:24 +1100

    chore(commitlint): allow arbitrary string for scope (#765)

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'footer-max-line-length': [0, 'always'], // disable the footer length limit
-    'scope-enum': [2, 'always', ['build', 'packages']],
+    'scope-enum': [0, 'always'], // allow arbitrary scope strings
     'scope-empty': [2, 'never']
   },
 };
