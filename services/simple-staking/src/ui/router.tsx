1e665eef1bfbf25c4901a65b4a0dc9ec6bd41988
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-12-06 00:25:26 -0500

    feat(packages): add standalone calculator

--- services/simple-staking/src/ui/router.tsx
+++ services/simple-staking/src/ui/router.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router";
 
 import BabyLayout from "./baby/layout";
+import CalculatorPage from "./common/calculator";
 import Layout from "./common/layout";
 import NotFound from "./common/not-found";
 import BTCStaking from "./common/page";
@@ -14,6 +15,7 @@ export const Router = () => {
         <Route path="btc" element={<BTCStaking />} />
         <Route path="baby" element={<BabyLayout />} />
         <Route path="rewards" element={<RewardsPage />} />
+        <Route path="calculator" element={<CalculatorPage />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
