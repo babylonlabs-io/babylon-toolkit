4acdcfe80f9e3538bbe5a40064a5c95e856d8f46
Crypto Minion <154598612+jrwbabylonlab@users.noreply.github.com>
2026-02-17 13:55:03 +1100

    chore(vault): remove the unused NEXT_PUBLIC_ENABLED_APPS (#1127)

--- services/vault/src/hooks/useApplications.ts
+++ services/vault/src/hooks/useApplications.ts
@@ -1,22 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { getAppIdByController, getEnabledAppIds } from "../applications";
+import { getAppIdByController } from "../applications";
 import { fetchApplications } from "../services/applications";
 
 export const APPLICATIONS_KEY = "applications";
 
 export const useApplications = () => {
-  const enabledAppIds = getEnabledAppIds();
-
   return useQuery({
-    queryKey: [APPLICATIONS_KEY, enabledAppIds],
+    queryKey: [APPLICATIONS_KEY],
     queryFn: async () => {
       const allApps = await fetchApplications();
 
-      return allApps.filter((app) => {
-        const appId = getAppIdByController(app.id);
-        return appId && enabledAppIds.includes(appId);
-      });
+      return allApps.filter((app) => getAppIdByController(app.id));
     },
   });
 };
