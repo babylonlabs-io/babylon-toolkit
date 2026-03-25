ec197fa1048e05a8dfd775c7da1dde565d11dffc
Govard Barkhatov <gbarkhatov@gmail.com>
2026-03-25 20:01:00 +0200

    chore(sdk): rc1

--- packages/babylon-ts-sdk/src/index.ts
+++ packages/babylon-ts-sdk/src/index.ts
@@ -3,4 +3,4 @@ export * from "./shared/index.js";
 export * from "./tbv/index.js";
 
 /** Test constant for RC publishing workflow validation. Remove after testing. */
-export const RC_TEST = "rc0" as const;
+export const RC_TEST = "rc1" as const;
