e6c008162564ab48525223e2ba58763557d96a1b
Govard Barkhatov <gbarkhatov@gmail.com>
2026-03-25 19:52:46 +0200

    feat(sdk): test rc publishing workflow 0

--- packages/babylon-ts-sdk/src/index.ts
+++ packages/babylon-ts-sdk/src/index.ts
@@ -1,3 +1,6 @@
 // Main SDK entry point
 export * from "./shared/index.js";
 export * from "./tbv/index.js";
+
+/** Test constant for RC publishing workflow validation. Remove after testing. */
+export const RC_TEST = "rc0" as const;
